c08fa979d8fadadfdf515c7ffdcb5bd504c83044
Volha Ivanova <Volha.Ivanova@itechart-group.com>
2023-06-04 11:24:18 +0300

    added lazy loading

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
+import { lazy, Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 
 import Layout from "./pages/Layout";
-import Home from "./pages/home/Home";
-import Menu from "./pages/menu/Menu";
-import Chefs from "./pages/chefs/Chefs";
-import About from "./pages/about/About";
-import Contacts from "./pages/contacts/Contacts";
 import "./App.css";
+
+const Home = lazy(() => import("./pages/home/Home"));
+const Menu = lazy(() => import("./pages/menu/Menu"));
+const Chefs = lazy(() => import("./pages/chefs/Chefs"));
+const About = lazy(() => import("./pages/about/About"));
+const Contacts = lazy(() => import("./pages/contacts/Contacts"));
 
 function App() {
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="/menu" element={<Menu />} />
-          <Route path="/chefs" element={<Chefs />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contacts" element={<Contacts />} />
-        </Route>
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="/menu" element={<Menu />} />
+            <Route path="/chefs" element={<Chefs />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contacts" element={<Contacts />} />
+          </Route>
+        </Routes>
+      </Suspense>
     </div>
   );
 }
--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,4 +1,3 @@
-import React from "react";
 import { NavLink, Outlet } from "react-router-dom";
 
 import noodleImage from "../assets/nav-images/noodle.png";
